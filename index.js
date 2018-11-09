54a45b877447e3046d4e042a2969460132cbd800
Mister-Kay <private-information>
2018-11-08 19:13:33 -0600

    add command talent, enhancement, or EP

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const Command = require('command')
 module.exports = function talentsDailyExpTracker(dispatch) {
     const command = Command(dispatch)
 
-    let info
+    let info = {}
+
+    command.add(['talent', 'talents', 'enhancement', 'enhancements', 'EP'], msg)
+
+    function msg() {
+        command.message(`Daily Talents Exp Cap: ${info.dailyExp}/${info.dailyExpMax} (${Math.round(100*info.dailyExp/info.dailyExpMax)}%)`)
+    }
 
     dispatch.hook('S_LOAD_EP_INFO', 1, (event)=>{
         info = event
     })
 
-    dispatch.hook('S_SHOW_USER_EP_INFO', 1, (event)=>{
-        command.message(`Daily Talents Exp Cap: ${info.dailyExp}/${info.dailyExpMax} (${Math.round(100*info.dailyExp/info.dailyExpMax)}%)`)
+    dispatch.hook('S_SHOW_USER_EP_INFO', 'raw', ()=>{
+        msg()
     })
 }
