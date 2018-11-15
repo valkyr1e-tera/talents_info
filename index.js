338ae0cf95a677b0c1adc44c97b6fa0cbea78eea
Mister-Kay <private-information>
2018-11-15 12:48:41 -0600

    actually works now

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
-const Command = require('command')
+//const Command = require('command')
 
 module.exports = function talentsDailyExpTracker(dispatch) {
-    const command = Command(dispatch)
+    //const command = Command(dispatch)
 
-    let info = {}
+    let exp = 0,
+        cap = 0
 
-    command.add(['talent', 'talents', 'enhancement', 'enhancements', 'EP'], msg)
+    // message on command
+    dispatch.command.add(['talent', 'talents', 'enhancement', 'enhancements', 'EP'], msg)
 
+    // send message exp/cap (exp%)
     function msg() {
-        command.message(`Daily Talents Exp Cap: ${info.dailyExp}/${info.dailyExpMax} (${Math.round(100*info.dailyExp/info.dailyExpMax)}%)`)
+        dispatch.command.message(`Daily Talents Exp Cap: ${exp}/${cap} (${Math.round(100*exp/cap)}%)`)
     }
 
+    // character log in
     dispatch.hook('S_LOAD_EP_INFO', 1, (event)=>{
-        info = event
+        exp = event.dailyExp
+        cap = event.dailyExpMax
     })
 
-    dispatch.hook('S_SHOW_USER_EP_INFO', 'raw', ()=>{
-        msg()
+    // change EP
+    dispatch.hook('S_PLAYER_CHANGE_EP', 'raw', (code, data)=>{
+        //exp = event.dailyExp
+        exp = data.readInt32LE(20)
+        //cap = event.dailyExpMax
+        cap = data.readInt32LE(24)
+        //msg()
+    })
+
+    // open EP ui
+    dispatch.hook('C_REQUEST_CONTRACT', 1, event => {
+        if (event.type == 77) {
+            msg()
+        }
     })
 }
