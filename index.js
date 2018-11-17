3897a077fa1664b29dc9e336557454bf4c969352
Owyn <Owyn@users.noreply.github.com>
2018-11-17 22:13:02 +0300

    SoftCap (89% of DailyCap) now used instead

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 module.exports = function talentsinfo(mod) {
 	const command = mod.command || mod.require.command;
+	const softcap = 0.8901403358192;
 	let warned = false;
 	let lvl = 0,
 		exp = 0,
 		dexp = 0,
-        dcap = 0;
+        dcap = 0,
+		sdcap = 0;
 
     // message on command
-    command.add(['talent', 'talents', 'EP'], msg);
+    command.add(['talent', 'talents', 'EP', '!EP'], msg);
 
     // send message exp/cap (exp%)
     function msg()
 	{
-        command.message(`<font color="#FDD017">Talents info:</font> LVL <font color="#00FFFF">${lvl}</font>, EXP: <font color="#00FFFF">${exp}</font>, DailyEXP <font color="#00FFFF">${dexp}/${dcap} (${Math.round(100*dexp/dcap)}%) </font>`);
+        command.message(`<font color="#FDD017">Talents info:</font> LVL <font color="#00FFFF">${lvl}</font>, EXP: <font color="#00FFFF">${exp}</font>, soft DailyEXP <font color="#00FFFF">${dexp}/${sdcap} (${Math.round(100*dexp/sdcap)}%) </font>`);
 	}
 	
 	mod.hook('S_LOAD_EP_INFO', 1, event=>{
@@ -20,21 +22,24 @@ module.exports = function talentsinfo(mod) {
 		lvl = event.level;
 		dexp = event.dailyExp;
 		dcap = event.dailyExpMax;
+		sdcap = Math.floor(dcap*softcap);
 	});
 	
 	mod.hook('S_PLAYER_CHANGE_EP', 'raw', (code, data)=>{
 		let gained = data.readInt32LE(4);
-		exp = data.readInt32LE(8); // 64 actually
+		exp = data.readInt32LE(8); // 64 actually but this should be enough
 		lvl = data.readInt32LE(16);
 		dexp = data.readInt32LE(20);
 		dcap = data.readInt32LE(24);
+		sdcap = Math.floor(dcap*softcap);
+		let scmod = Math.round(data.readFloatLE(37) * 100);
 		if(gained)
 		{
-			if(dexp >= dcap)
+			if(dexp >= sdcap)
 			{
 				if(!warned)
 				{
-					command.message('<font color="#FDD017">Talents EXP</font> DailyCap <font color="#FF0000">reached!</font>');
+					command.message('<font color="#FDD017">Talents EXP</font> Daily SoftCap <font color="#FF0000">reached!</font>');
 					warned = true;
 				}
 			}
@@ -42,7 +47,7 @@ module.exports = function talentsinfo(mod) {
 			{
 				warned = false;
 			}
-			command.message('<font color="#FDD017">Talents:</font> <font color="#00FFFF">+' + gained + ' EXP</font>' + (!warned ? ' (' + dexp + ' / ' + dcap + ' (DailyCap), <font color="#FFF380">' + (dcap-dexp) + '</font> left for today)' : '' ));
+			command.message('<font color="#FDD017">Talents:</font> <font color="#00FFFF">+' + gained + ' EXP</font>' + (!warned ? ' (' + dexp + ' / ' + sdcap + ' (Daily SoftCap), <font color="#FFF380">' + (sdcap-dexp) + '</font> left for today uncapped)' : ' (' + scmod + '% mod)' ));
 		}
 	});
 	
